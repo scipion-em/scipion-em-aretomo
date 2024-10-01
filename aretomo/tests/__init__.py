f3b0484f463d12e7f23edfe3e815539c660fed47
JorMaister <jorgejjs86@gmail.com>
2024-10-01 19:06:45 +0200

    Add new test dataset description

--- aretomo/tests/__init__.py
+++ aretomo/tests/__init__.py
@@ -23,8 +23,11 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
+from enum import Enum
 
 from pyworkflow.tests import DataSet
+from tomo.objects import TomoAcquisition
+
 
 DataSet(name='tomo-em',
         folder='tomo-em',
@@ -32,3 +35,44 @@ DataSet(name='tomo-em',
             'ts1': 'tutorialData/BBa.st',
             'ts2': 'tutorialData/BBb.st'
         })
+
+########################################################################################################################
+EMDB_10453 = 'empiar_10453'
+
+TS_079 = 'TS_079'
+TS_145 = 'TS_145'
+testAcq = TomoAcquisition(voltage=300,
+                          sphericalAberration=2.7,
+                          amplitudeContrast=0.1,
+                          magnification=50000,
+                          doseInitial=0,
+                          tiltAxisAngle=84.8,
+                          step=3)
+# Acquisition of TS_079
+testAcq079 = testAcq.clone()
+testAcq079.setAngleMin(-60.005)
+testAcq079.setAngleMax(59.9828)
+testAcq079.setDosePerFrame(3.55679)
+testAcq079.setAccumDose(138.71481)
+# Acquisition of TS_145
+testAcq145 = testAcq.clone()
+testAcq145.setAngleMin(-60.007)
+testAcq145.setAngleMax(59.9848)
+testAcq145.setDosePerFrame(3.5424)
+testAcq145.setAccumDose(138.1536)
+
+
+class DataSetEmpiar10453(Enum):
+    unbinnedPixSize = 1.33
+    tsDims = [5760, 4092, 41]
+    nAngles = 41
+    testAcqDict = {TS_079: testAcq079,
+                   TS_145: testAcq145}
+
+    @classmethod
+    def getInterpTsDims(cls, binningFactor=1, nImgs=-1):
+        return [cls.tsDims[0] / binningFactor,
+                cls.tsDims[1] / binningFactor,
+                nImgs]
+
+DataSet(name=EMDB_10453, folder=EMDB_10453, files={el.name: el.value for el in DataSetEmpiar10453})
